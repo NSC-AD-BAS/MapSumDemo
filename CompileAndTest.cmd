03d06aac10763220ca0470c1756b006cf25deaff
Bob McHenry <bob.mchenry@slalom.com>
2018-02-13 15:49:18 -0800

    Fixing issue from line ending conversion

--- CompileAndTest.cmd
+++ CompileAndTest.cmd
@@ -3,8 +3,7 @@ javac .\ad325\mapSumProblem\mapSumClasses\*.java
 javac .\ad325\mapSumProblem\mapSumTests\*.java
 
 ECHO ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-ECHO 
-Your Test Results!
+ECHO Your Test Results!
 
 java  ad325.mapSumProblem.mapSumTests.TestRunner
 
